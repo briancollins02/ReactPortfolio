0833d85f31ff9bc59b57457529982a5dd2e88f9d
briancollins02 <brianmcollins0@gmail.com>
2023-02-18 16:13:43 -0500

    create the css for the portfolio tab

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function About() {
+    return (
+        <div>
+            <ul>
+                <li>contact1</li>
+                <li>contact2</li>
+                <li>contact3</li>
+            </ul>
+        </div>
+    )
+}
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -3,11 +3,70 @@ import React from 'react';
 export default function Portfolio() {
     return (
         <div>
-            <h1>Portfolio</h1>
-            <img></img>
-            <p>
-                portfolio elements
-            </p>
+            <main>
+                <div class='content-container'>
+                    <div className="project-container">
+                        <div class='image-container'>
+                            <img className="project-img" src={process.env.PUBLIC_URL + "/images/NoteTaker.png"} alt="Note-Taker Application" />
+                        </div>
+                        <div class='link-container'>
+                            <a className="project-github" href="https://github.com/briancollins02/challenge11-ExpressNotes/"> <img className="github-logo" 
+                            src={process.env.PUBLIC_URL + "/images/github-link.png"} alt="Github Logo" />  </a>
+                            <a className="project-deploy" href="https://warm-stream-37708.herokuapp.com"><p> Note Taker </p></a>
+                        </div>
+                    </div>
+                    <div className="project-container">
+                        <div class='image-container'>
+                            <img className="project-img" src={process.env.PUBLIC_URL + "/images/NoteTaker.png"} alt="Note-Taker Application" />
+                        </div>
+                        <div class='link-container'>
+                            <a className="project-github" href="https://github.com/briancollins02/challenge11-ExpressNotes/"> <img className="github-logo" 
+                            src={process.env.PUBLIC_URL + "/images/github-link.png"} alt="Github Logo" />  </a>
+                            <a className="project-deploy" href="https://warm-stream-37708.herokuapp.com"><p> Note Taker </p></a>
+                        </div>
+                    </div>
+                    <div className="project-container">
+                        <div class='image-container'>
+                            <img className="project-img" src={process.env.PUBLIC_URL + "/images/NoteTaker.png"} alt="Note-Taker Application" />
+                        </div>
+                        <div class='link-container'>
+                            <a className="project-github" href="https://github.com/briancollins02/challenge11-ExpressNotes/"> <img className="github-logo" 
+                            src={process.env.PUBLIC_URL + "/images/github-link.png"} alt="Github Logo" />  </a>
+                            <a className="project-deploy" href="https://warm-stream-37708.herokuapp.com"><p> Note Taker </p></a>
+                        </div>
+                    </div>
+                    <div className="project-container">
+                        <div class='image-container'>
+                            <img className="project-img" src={process.env.PUBLIC_URL + "/images/NoteTaker.png"} alt="Note-Taker Application" />
+                        </div>
+                        <div class='link-container'>
+                            <a className="project-github" href="https://github.com/briancollins02/challenge11-ExpressNotes/"> <img className="github-logo" 
+                            src={process.env.PUBLIC_URL + "/images/github-link.png"} alt="Github Logo" />  </a>
+                            <a className="project-deploy" href="https://warm-stream-37708.herokuapp.com"><p> Note Taker </p></a>
+                        </div>
+                    </div>
+                    <div className="project-container">
+                        <div class='image-container'>
+                            <img className="project-img" src={process.env.PUBLIC_URL + "/images/NoteTaker.png"} alt="Note-Taker Application" />
+                        </div>
+                        <div class='link-container'>
+                            <a className="project-github" href="https://github.com/briancollins02/challenge11-ExpressNotes/"> <img className="github-logo" 
+                            src={process.env.PUBLIC_URL + "/images/github-link.png"} alt="Github Logo" />  </a>
+                            <a className="project-deploy" href="https://warm-stream-37708.herokuapp.com"><p> Note Taker </p></a>
+                        </div>
+                    </div>
+                    <div className="project-container">
+                        <div class='image-container'>
+                            <img className="project-img" src={process.env.PUBLIC_URL + "/images/NoteTaker.png"} alt="Note-Taker Application" />
+                        </div>
+                        <div class='link-container'>
+                            <a className="project-github" href="https://github.com/briancollins02/challenge11-ExpressNotes/"> <img className="github-logo" 
+                            src={process.env.PUBLIC_URL + "/images/github-link.png"} alt="Github Logo" />  </a>
+                            <a className="project-deploy" href="https://warm-stream-37708.herokuapp.com"><p> Note Taker </p></a>
+                        </div>
+                    </div>
+                </div>   
+            </main>
         </div>
     )
 }
